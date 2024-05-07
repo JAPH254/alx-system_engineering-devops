06c0cb982bc71e5e892a0a6e0d76bedd114a0287
JAPH254 <109793045+JAPH254@users.noreply.github.com>
2024-05-07 16:43:34 +0300

    Update 1-top_ten.py

--- 0x16-api_advanced/1-top_ten.py
+++ 0x16-api_advanced/1-top_ten.py
@@ -1,19 +1,22 @@
 #!/usr/bin/python3
-''' task 1 module'''
-
+"""Contains top_ten function"""
 import requests
-import sys
 
 
 def top_ten(subreddit):
-    '''gets 10 hottest posts of a subreddit'''
-    headers = {'User-agent': 'test23'}
-    url = 'https://www.reddit.com/r/'
-    posts = requests.get(url + '{}/hot.json?limit=10'.format(
-        sys.argv[1]), allow_redirects=False, headers=headers)
-
-    if posts.status_code == 200:
-        for post in posts.json()['data']['children']:
-            print(post['data']['title'])
-    else:
-        print(None)
+    """Print the titles of the 10 hottest posts on a given subreddit."""
+    url = "https://www.reddit.com/r/{}/hot/.json".format(subreddit)
+    headers = {
+        "User-Agent": "0x16-api_advanced:project:\
+v1.0.0 (by /u/firdaus_cartoon_jr)"
+    }
+    params = {
+        "limit": 10
+    }
+    response = requests.get(url, headers=headers, params=params,
+                            allow_redirects=False)
+    if response.status_code == 404:
+        print("None")
+        return
+    results = response.json().get("data")
+    [print(c.get("data").get("title")) for c in results.get("children")]
